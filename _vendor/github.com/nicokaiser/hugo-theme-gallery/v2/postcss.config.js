fd3ae3c50efb32fecb79a9b526b2fd92be0086a4
Nico Kaiser <nico@kaiser.me>
2024-01-25 15:29:30 +0100

    remove vendor directory

--- _vendor/github.com/nicokaiser/hugo-theme-gallery/v2/postcss.config.js
+++
@@ -1,8 +0,0 @@
-const atImport = require("postcss-import");
-const tailwind = require("tailwindcss")(process.env.HUGO_FILE_TAILWIND_CONFIG_JS || "./tailwind.config.js");
-const autoprefixer = require("autoprefixer");
-
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: [atImport, tailwind, autoprefixer],
-};
